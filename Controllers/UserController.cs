0b2bfd1b8baf48f4a2abac69935b1382a6543ce8
ShabanaNivade <shabananivade@gmail.com>
2025-05-16 19:34:38 +0530

    Add project files.

---
+++ Controllers/UserController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Shabana.DBFirst.DataAccessLayer;
+using Shabana.DBFirst.DataAccessLayer.Models;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[Controller]/[action]")]
+    [ApiController]
+    public class UserController : Controller
+    {
+       QuickKartRepository quickKartRepository;
+
+        public UserController(QuickKartRepository quickKartRepository)
+        {
+            this.quickKartRepository = quickKartRepository;
+        }
+        [HttpGet]
+        public JsonResult GetAllUser()
+        {
+            List<User> users = new List<User>();
+            try
+            {
+                users = quickKartRepository.GetUsers();
+            }
+            catch (Exception ex)
+            {
+                users = null;
+            }
+            return Json(users);
+        }
+        [HttpGet]
+        public JsonResult GetUser(string EmailID)
+        {
+            User userbyEmail = new User ();
+            try
+            {
+                userbyEmail = quickKartRepository.GetUserByEmailId(EmailID);
+
+            }
+            catch (Exception ex)
+            {
+                userbyEmail = null;
+            }
+            return Json(userbyEmail);
+
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,43 @@
+
+using Shabana.DBFirst.DataAccessLayer;
+using Shabana.DBFirst.DataAccessLayer.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace WebAPI
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+            builder.Services.AddTransient<QuickKartContext>();
+            builder.Services.AddTransient<QuickKartRepository>(
+                c => new QuickKartRepository(c.GetRequiredService<QuickKartContext>())); 
+
+            // Add services to the container.
+
+            builder.Services.AddControllers();
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
